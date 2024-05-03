053e17504497bd8dddc67c7e775a3f823bfd290b
ThisIsHD <himadridey165@gmail.com>
2024-05-03 22:27:27 +0530

    added to components

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -26,21 +26,27 @@ function Navbar() {
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
       <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+        <a>Home</a>
+        </li>
+        <li>
+        <a>Course</a>
+        </li>
+        <li>
+        <a>Contact</a>
+        </li>
+        <li>
+        <a>About</a>
+        </li>
+      
     </ul>
   </div>
+  <label className="input input-bordered flex items-center gap-2">
+  <input type="text" className="grow" placeholder="Search" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+</label>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <a className="btn">Login</a>
   </div>
 </div>
     </div>
