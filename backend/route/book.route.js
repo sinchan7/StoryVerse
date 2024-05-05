847398a55d73781f53d5ab47ba4fcf1124fb6227
ThisIsHD <himadridey165@gmail.com>
2024-05-05 11:13:31 +0530

    backend

--- backend/route/book.route.js
+++ backend/route/book.route.js
@@ -1,5 +1,5 @@
-import express from "express";
-import { getBook } from "../controller/book.controller";
+import express from "express"
+import {getBook} from "../controller/book.controller.js"
 
 const router=express.Router()
 router.get("/",getBook);
