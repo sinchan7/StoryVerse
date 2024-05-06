e9d17ba06b5b3df84197e3744478e8f9b03d7eb6
ThisIsHD <himadridey165@gmail.com>
2024-05-06 10:04:49 +0530

    aa

---
+++ frontend/components/Bestsellers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Bestsellers() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Bestsellers
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import Banner from '../components/Banner'
-import Freebook from '../components/Freebook'
-
+import Home from '../home/Home'
+import Bestsellers from '../components/Bestsellers'
+import {Route,Routes} from "react-dom"
 
 function App() {
-  return ( <>
-  <Navbar/>
-  <Banner/>
-  <Footer/>
-  <Freebook/>
-  </>
-    
+  return (
+    <>
+    {/*<Home/>
+    <Bestsellers/>*/}
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/Bestsellers" element={<Bestsellers/>}/>
+    </Routes>
+    </>
   )
 }
 
