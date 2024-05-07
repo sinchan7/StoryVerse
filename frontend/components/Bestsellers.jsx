797306a30970d36dfa44fa0b5f1cfb18644e8f09
ThisIsHD <himadridey165@gmail.com>
2024-05-07 12:19:50 +0530

    freebook

--- frontend/components/Bestsellers.jsx
+++ frontend/components/Bestsellers.jsx
@@ -8,12 +8,12 @@ function Bestsellers() {
     <div className='max-w-screen-2x1 container mx-auto md:px-20 px-4'>
       <div className='mt-28 items-center justify-center text-center'></div>
       <div>
-        <h1 className='text-2x1 font-semibold md:text-8x1'>Delighted to Have you here!</h1>
+        <h1 className='text-20x10 font-semibold md:text-80x10'>Delighted to Have you here!</h1>
         <p className='mt-12'>
         A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long.
         </p>
-        <button className='-mt-6 bg-pink-500'>
-          back
+        <button className='bg-blue-500 text-white rounded-md px-5 py-1.5'>
+          Back
         </button>
         <div className='mt-12 grid-cols-1 md:grid-cols-4'>
           {
--- frontend/home/Home.jsx
+++ frontend/home/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Banner from '../components/Banner'
-import Footer from '../components/Footer'
 import Freebook from '../components/Freebook'
+import Footer from '../components/Footer'
 
 function home() {
   return (
     <>
     <Navbar/>
     <Banner/>
-    <Footer/>
     <Freebook/>
+    <Footer/>
     </>
   )
 }
