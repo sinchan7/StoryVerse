78b23bcd85f69496319d5b96bba0bfce591c326f
ThisIsHD <himadridey165@gmail.com>
2024-05-08 00:18:49 +0530

    abcd

--- frontend/components/Bestsellers.jsx
+++ frontend/components/Bestsellers.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Cards from './Cards'
 
+
 function Bestsellers() {
   const [book,setBook]=useState([])
   useEffect(() =>{
@@ -19,10 +20,10 @@ function Bestsellers() {
   return (
     <>
     <div className='max-w-screen-2x1 container mx-auto md:px-20 px-4'>
-      <div className='mt-28 items-center justify-center text-center'></div>
+      <div className='mt--20 items-center justify-center text-center'></div>
       <div>
         <h1 className='text-20x10 font-semibold md:text-80x10'>Delighted to Have you here!</h1>
-        <p className='mt-12'>
+        <p className=''>
         A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long.
         </p>
         <button className='bg-blue-500 text-white rounded-md px-5 py-1.5'>
@@ -33,6 +34,7 @@ function Bestsellers() {
             book.map((item)=>(
               <Cards key={item.id} item={item}/>
             ))
+            
 
           }
         </div>
