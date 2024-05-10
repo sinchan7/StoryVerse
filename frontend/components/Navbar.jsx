97a633008dae958e4fe5f736aa36b5fcdce38575
Abhi-rup-Ray <abhirupray.abhi@gmail.com>
2024-05-10 21:30:25 +0530

    The Ultimate Changes

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import Login from './Login'
+import Logout from './Logout'
+import { useAuth } from '../src/context/AuthProvider'
 
 function Navbar() {
+
+  const [authUser,setAuthUser]=useAuth()
+  console.log(authUser)
+
   return (
     <>
     <div>
@@ -46,13 +52,17 @@ function Navbar() {
   <input type="text" className="grow" placeholder="Search" />
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
 </label>
-  <div className="navbar-end">
+
+    {
+      authUser?<Logout/>:
+    <div className="navbar-end">
     <a className="btn"
     onClick={()=>
       document.getElementById("my_modal_3").showModal()
     }>Login</a>
     <Login/>
   </div>
+  }
 </div>
     </div>
     </>
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import Home from '../home/Home'
-import {Route,Routes} from 'react-router-dom'
+import {Navigate, Route,Routes} from 'react-router-dom'
 import Bestsellers from '../components/Bestsellers'
 import Signup from '../components/Signup'
+import { Toaster } from 'react-hot-toast'
+import { useAuth } from './context/AuthProvider'
 
 
 function App() {
+  const [authUser,setAuthUser]=useAuth();
+  console.log(authUser);
   return (
     <>
     {/*<Home/>
     <Bestsellers/>*/}
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/Bestsellers" element={<Bestsellers/>}/>
+      <Route path="/Bestsellers" element={authUser?<Bestsellers/>:<Navigate to="/signup"/>} />
       <Route path="/Signup" element={<Signup/>}/>
       </Routes>
+      <Toaster />
     </>
  )
 }
